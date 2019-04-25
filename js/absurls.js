c7eaa7507e2891e54c8c254ae1a19a33f2562d52
Travis CI User <travis@example.org>
2019-04-25 15:26:56 +0000

    rebuilding site Thu Apr 25 15:26:56 UTC 2019

--- js/absurls.js
+++
@@ -1,5 +0,0 @@
-if(!window.location.hostname.includes('digital-land.github.io') && !window.location.hostname.includes('localhost')) {
-	document.querySelectorAll("[href^='/']").forEach(function(element) {
-		element.setAttribute('href', 'https://' + window.location.hostname + '/digital-land.github.io/' + element.getAttribute('href'));
-	});
-}
